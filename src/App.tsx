ebe6c30e7f54c20704d59fd96a78eb7ba31dd60f
0xthecableguy <0xthecableguy@proton.me>
2024-11-19 02:14:57 +0300

    Client-server base functionality implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,42 @@
 import React, { useState } from 'react';
 import ChatBox from './components/ChatBox';
 import './App.css';
-import {sendMessageToServer} from "./services/api";
-
-export interface ServerResponse {
-    message: string;
-    buttons?: string[];
-}
+import { sendMessageToServer } from "./services/api";
 
 const App = () => {
     const [messages, setMessages] = useState<string[]>([]);
     const [buttons, setButtons] = useState<string[]>([]);
+    const [actionButtons, setActionButtons] = useState<string[]>([]);// Массив для кнопок, сгенерированных сервером
 
     const handleSendMessage = async (message: string) => {
-        setMessages([...messages, message]);
+        setMessages((prevMessages) => [...prevMessages, `Вы: ${message}`]);
 
         try {
-            const { message: serverMessage, buttons: serverButtons } = await sendMessageToServer(message);
-            setMessages((prevMessages) => [...prevMessages, `Server's response: ${serverMessage}`]);
+            const serverResponse = await sendMessageToServer(message);
 
-            if (serverButtons) {
-                setButtons(serverButtons); // Обновляем кнопки, если они есть в ответе
-            } else {
-                setButtons([]); // Очищаем кнопки, если ответ не содержит информации о них
-            }
+            // Обновляем сообщения
+            setMessages((prevMessages) => [
+                ...prevMessages,
+                `Ответ: ${serverResponse.message}`,
+            ]);
+
+            // Обновляем кнопки
+            setButtons(serverResponse.buttons || []); // Устанавливаем основные кнопки
+            setActionButtons(serverResponse.action_buttons || []); // Устанавливаем кнопки действий (Назад, Выход)
         } catch (error) {
-            console.error('Error sending message:', error);
+            console.error('Ошибка отправки сообщения:', error);
         }
     };
 
     return (
         <div className="App">
             <h1 className="chat-header">reQuest App</h1>
-            <ChatBox messages={messages} onSendMessage={handleSendMessage} buttons={buttons} />
+            <ChatBox
+                messages={messages}
+                onSendMessage={handleSendMessage}
+                buttons={buttons}
+                actionButtons={actionButtons}
+            />
         </div>
     );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,15 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    proxy: {
+      // перенаправление всех запросов с /user_action на ваш сервер
+      '/user_action': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 });
