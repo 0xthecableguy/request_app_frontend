3ec28099bd663669e63981cd253fcee95be6cbf0
0xthecableguy <0xthecableguy@proton.me>
2024-11-22 00:09:09 +0300

    new assemble flow

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/user_action': {
-        target: 'http://localhost:3000',
+        target: 'http://195.200.18.50:3000',
         changeOrigin: true,
         secure: false,
       }
